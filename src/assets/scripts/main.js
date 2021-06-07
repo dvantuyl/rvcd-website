d844768761761d1880d28e94026bdeca277452dd
Dwight VanTuyl <dvantuyl@gmail.com>
2021-06-07 09:30:33 -0700

    Events | RVCD Planning Meeting

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -37,25 +37,6 @@ document.addEventListener('DOMContentLoaded', function () {
     dayMaxEvents: true, // allow "more" link when too many events
     events: [
       {
-        title: 'All Day Event',
-        start: '2021-04-12',
-      },
-      {
-        title: 'Long Event',
-        start: '2021-04-07',
-        end: '2021-04-10',
-      },
-      {
-        groupId: 999,
-        title: 'Repeating Event',
-        start: '2021-04-09T16:00:00',
-      },
-      {
-        groupId: 999,
-        title: 'Repeating Event',
-        start: '2021-04-16T16:00:00',
-      },
-      {
         title: 'Conference',
         start: '2021-04-11',
         end: '2021-04-13',
@@ -66,29 +47,9 @@ document.addEventListener('DOMContentLoaded', function () {
         end: '2021-04-12T12:30:00',
       },
       {
-        title: 'Lunch',
-        start: '2021-04-12T12:00:00',
-      },
-      {
-        title: 'Meeting',
-        start: '2021-04-12T14:30:00',
-      },
-      {
-        title: 'Happy Hour',
-        start: '2021-04-12T17:30:00',
-      },
-      {
-        title: 'Dinner',
-        start: '2021-04-12T20:00:00',
-      },
-      {
-        title: 'Birthday Party',
-        start: '2021-04-13T07:00:00',
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2021-04-28',
+        title: 'RVCD Planning Meeting',
+        url: '/posts/planning-meeting-20210608',
+        start: '2021-06-08',
       },
     ],
   })
